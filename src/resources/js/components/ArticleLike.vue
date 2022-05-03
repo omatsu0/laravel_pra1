870580dd0ac2e02b3082d40910fec1331dd8ef49
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2022-05-04 00:00:45 +0900

    .

--- src/resources/js/components/ArticleLike.vue
+++ src/resources/js/components/ArticleLike.vue
@@ -36,6 +36,7 @@
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
+        gotToLike: false,
       }
     },
     methods: {
@@ -54,12 +55,14 @@
 
         this.isLikedBy = true
         this.countLikes = response.data.countLikes
+        this.gotToLike = true
       },
       async unlike() {
         const response = await axios.delete(this.endpoint)
 
         this.isLikedBy = false
         this.countLikes = response.data.countLikes
+        this.gotToLike = false
       },
     },
   }
